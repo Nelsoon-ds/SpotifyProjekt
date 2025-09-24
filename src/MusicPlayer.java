cade5470d12cf2482e0e7c18b0717de4d1b38ad0
Nelsoon-ds <davidgaming108@gmail.com>
2025-09-24 12:51:43 +0200

    Tilføjet lidt ekstra

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -26,26 +26,18 @@ public class MusicPlayer {
 
     }
 
-    // 0 Diskuter flowet af programmet
-    // 1 UML-Diagram
-    // 2 Fyld metoderne ud
-
-
     /**
      * <p>
      *     This is the main entry point of the program
      * </p>
      */
     public void startProgram() {
-        System.out.println("**************************************");
-        System.out.println("Welcome to your Fake Spotify Service!");
-        System.out.println("**************************************");
-        System.out.println("Press 1 to add a song.\nPress 2 to delete a song.\nPress 3 to search for a song.\nPress 4 to edit song." +
-                "\nPress 5 to clear the playlist.\nPress 6 to play songs.\nPress 7 to end the program.");
-        System.out.println("**************************************");
+        // Initialize variables
+        boolean isDone = false;
+        printMenu();
 
+        while (!isDone) {
         int userChoice = scan.nextInt();
-
         switch (userChoice) {
             case 1:
                 addSong();
@@ -64,18 +56,36 @@ public class MusicPlayer {
                 break;
             case 6:
                playSong();
-                // Print all songs
                 break;
             case 7:
+                isDone = true;
                 endProgram();
-                // Exit program
                 break;
         }
+            }
     }
 
     public void playSong() {
-        System.out.println(playList.toString());
-    };
+        // skal have brugerens input til at vælge en sang
+        printPlaylist();
+        System.out.println("Choose a song to play: ");
+        int userInput = scan.nextInt();
+        // To ensure it doesent eat up
+        scan.nextLine();
+        System.out.println(playList.get(userInput));
+        // print menuen for at få brugeren tilbage
+        printMenu();
+    }
+
+    public void printPlaylist() {
+        System.out.println("*****************");
+        System.out.println("Songlist");
+        System.out.println("*****************");
+        for (int i = 0; i < playList.size(); i++) {
+            System.out.println("ID:" + i + "\n" + playList.get(i));
+        }
+        System.out.println("**************** ");
+    }
 
     public void addSong() {
         String userGenre;
@@ -87,29 +97,35 @@ public class MusicPlayer {
         // Ask the user for a genre
         System.out.println("Please enter the genre of the song: ");
         userGenre = scan.next();
-        System.out.println(userGenre.toUpperCase());
-        // Convert the String to enum
-        genreEnum = Genre.valueOf(userGenre);
-        // Add the song to the playlist by calling playList.add
-        Song userSong = new Song(songTitle, genreEnum);
-        playList.add(userSong);
-        // Tell the user the song has been added
-        System.out.println("Song added with title: " + songTitle + " and genre: " + genreEnum);
+        try {
+            // Convert the String to enum
+            genreEnum = Genre.valueOf(userGenre.toUpperCase());
+            // Add the song to the playlist by calling playList.add
+            Song userSong = new Song(songTitle, genreEnum);
+            playList.add(userSong);
+            // Tell the user the song has been added
+            System.out.println("Song added with title: " + songTitle + " and genre: " + genreEnum);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid genre entered. Please try again with a valid genre.");
+        }
+        printMenu();
+
     }
     private void deleteSong() {
-        System.out.print("Indtast titlen på sangen der skal fjernes: ");
-        String titel = scan.nextLine();
+        System.out.print("Insert the tile of the song that should be removed: ");
+        String title = scan.nextLine();
 
-        for (int i = 0; i < playList.size(); i++) {
-            if (playList.get(i).getTitle().equalsIgnoreCase(titel)) {
-                System.out.println("🗑️ Sletning: " + playList.get(i));
-                playList.remove(i);
-                System.out.println("✅ Sangen er fjernet.");
-                return;
+
+        boolean removed = playList.removeIf(song -> song.getTitle().equalsIgnoreCase(title));
+        if (removed) {
+            System.out.println(title + " was removed from the playlist.") ;
             }
-        }
-        System.out.println("🚫 Sangen blev ikke fundet.");
+        else {
+            System.out.println(title + " was not found on the playlist.");
+            }
+
     }
+
 
 
     public void clearPlaylist() {
@@ -118,23 +134,42 @@ public class MusicPlayer {
     }
 
     public void editSong () {
+        // Vælg nummeret på den plads det skal ændres
         System.out.println("Choose the number of the song in the playlist you want to edit: ");
-        int numberInPlaylist = scan.nextInt() - 1;
+        int numberInPlaylist = scan.nextInt();
         scan.nextLine();
         System.out.println("What is the title of the new song? ");
         String titleOfSong = scan.nextLine();
-        System.out.println("What is the genre of the new song? ");
-        String genreInput = scan.nextLine().toUpperCase();
-        Genre genreOfSong = Genre.valueOf(genreInput);
-        Song newSong = new Song(titleOfSong,genreOfSong);
-        playList.set(numberInPlaylist, newSong);
-        System.out.println("Your song " + titleOfSong + " has been added to #" + numberInPlaylist + 1 + " spot on your playlist" );
+        System.out.println(numberInPlaylist);
+
+        try {
+            System.out.println("What is the genre of the new song? ");
+            String genreInput = scan.nextLine().toUpperCase();
+            Genre genreOfSong = Genre.valueOf(genreInput);
+            Song newSong = new Song(titleOfSong,genreOfSong);
+            playList.set(numberInPlaylist, newSong);
+            System.out.println("Your song " + titleOfSong + " has been added to #" + numberInPlaylist  + " spot on your playlist" );
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Not eligible genre");
+        }
+
+
         // playList.set();
     }
     public void searchSong() {
         //playList.get();
     }
     public void endProgram() {
-        //
+        System.exit(0) ;
+    }
+
+    public void printMenu() {
+        System.out.println("**************************************");
+        System.out.println("Welcome to your Fake Spotify Service!");
+        System.out.println("**************************************");
+        System.out.println("Press 1 to add a song.\nPress 2 to delete a song.\nPress 3 to search for a song.\nPress 4 to edit song." +
+                "\nPress 5 to clear the playlist.\nPress 6 to play songs.\nPress 7 to end the program.");
+        System.out.println("**************************************");
     }
 }
